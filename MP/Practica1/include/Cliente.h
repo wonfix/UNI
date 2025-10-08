2982587c3b096dde5c7f68cf97f00a84597da237
wonfix <pablogarciacampanario95@gmail.com>
2025-10-08 20:23:40 +0200

    Practica1 de MP

--- MP/Practica1/include/Cliente.h
+++ MP/Practica1/include/Cliente.h
@@ -1,17 +1,32 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
+#include <iostream>
+#include "Fecha.h"
 
-class Cliente
-{
-private:
-    long int dni;
-    char *nombre;
+using namespace std;
+
+class Cliente {
+  long int dni;
+  char *nombre;
+  Fecha fechaAlta;
 public:
+  Cliente(long int d, char *nom, Fecha f);
 
+  virtual ~Cliente();
+  Cliente& operator=(const Cliente& c);
 
-    Cliente();
-    virtual ~Cliente();
+  long int getDni() const { return this->dni; }
+  const char* getNombre() const { return nombre; } //VIP devolver un puntero constante para evitar que desde el main() se puede modificar el nombre
+  Fecha getFecha() const { return fechaAlta; }
+
+  void setNombre(char *nom);
+  void setFecha(Fecha f);
+
+  bool operator==(Cliente c) const; // if (c1 ===c2)
+  Cliente(const Cliente &c);
+  friend ostream& operator<<(ostream &s, const Cliente &c);
 };
 
-#endif // CLIENTE_H
+
+#endif
